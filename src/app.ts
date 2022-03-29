c1bf2f366cc1535de50fe356effdb4c8c9bdc585
gehmac <gmarcoscardoso@gmail.com>
2022-03-29 18:04:36 -0300

    feat: add route "products"

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,17 @@
 import express from 'express';
+import ProductController from './controllers/products.constroller';
+
+const productController = new ProductController();
 
 const app = express();
-
 app.use(express.json());
+
+app
+  .route('/')
+  .get((req, res) => res.status(200).json({ message: 'ok' }));
+
+app
+  .route('/products')
+  .get(productController.getAll);
 
 export default app;
