534694e6301c29322ff3f8d0e4cfb7c0239d8891
gehmac <gmarcoscardoso@gmail.com>
2022-03-30 18:39:15 -0300

    feat: create products

--- src/controllers/products.constroller.ts
+++ src/controllers/products.constroller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import ProductService from '../services/products.service';
+import validateProducts from '../schemas/validateProducts';
 
 export default class ProductController {
   constructor(private productService = new ProductService()) {}
@@ -7,5 +8,19 @@ export default class ProductController {
   public getAll = async (req: Request, res: Response) => {
     const products = await this.productService.getAll();
     return res.status(200).json(products);
+  };
+
+  public create = async (req: Request, res: Response) => {
+    const product = req.body;
+
+    const { error } = validateProducts.validate(product);
+    if (error) {
+      const [code, data] = error.message.split('|');
+
+      return res.status(Number(code)).json({ error: data });
+    }
+
+    const productCreate = await this.productService.create(product);
+    return res.status(201).json({ item: productCreate });
   };
 }
--- src/models/products.model.ts
+++ src/models/products.model.ts
@@ -1,4 +1,4 @@
-import { Pool } from 'mysql2/promise';
+import { Pool, ResultSetHeader } from 'mysql2/promise';
 import { Product } from '../interfaces';
 
 export default class ProductModel {
@@ -11,5 +11,15 @@ export default class ProductModel {
   public async getAll(): Promise<Product[]> {
     const [result] = await this.connection.execute('SELECT * FROM Trybesmith.Products');
     return result as Product[];
+  }
+
+  public async create(product: Product): Promise<Product> {
+    const { name, amount } = product;
+    const [{ insertId }] = await this.connection.execute<ResultSetHeader>(
+      'INSERT INTO Trybesmith.Products (name, amount) VALUES (?, ?)',
+      [name, amount],
+    );
+
+    return { id: insertId, ...product };
   }
 }
